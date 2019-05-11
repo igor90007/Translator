e1291676a121893d28e73ce552735682afb4111d
igor90007 <igor90007@gmail.com>
2019-05-11 17:48:09 +0300

    add voice recognize library / add minimal ui for testing recognize feature

--- src/containers/General/Main.tsx
+++ src/containers/General/Main.tsx
@@ -1,13 +1,44 @@
 import React from 'react'
+import Voice from 'react-native-voice'
 
-import Screen from '../../screens/General/Main'
+import { Button } from 'react-native'
 
 export default class MainContainer extends React.Component {
-  render() {
-    return <Screen />
+  constructor(props: object) {
+    super(props)
+    Voice.onSpeechResults = this.onSpeechResultsHandler.bind(this)
   }
 
-  private testFunc = () => {
-    //
+  componentWillUnmount() {
+    Voice.destroy().then(Voice.removeAllListeners)
+  }
+
+  render() {
+    return (
+      <>
+        <Button onPress={this.onStartButtonPress} title="Start recognize" />
+        <Button onPress={this.onStopPress} title="Stop recognize" />
+      </>
+    )
+  }
+
+  private async onStartButtonPress() {
+    try {
+      await Voice.start('en-US')
+    } catch (e) {
+      console.log(e, 'onStartButtonPressErr')
+    }
+  }
+
+  private async onStopPress() {
+    try {
+      await Voice.stop()
+    } catch (e) {
+      console.log(e, 'onStopPressErr')
+    }
+  }
+
+  private onSpeechResultsHandler = (event: object) => {
+    console.log(event, 'onSpeechResultsHandler')
   }
 }
